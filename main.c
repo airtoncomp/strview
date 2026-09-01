4ee8dce3c5632279be8807ee3f6371943d9b7ea2
airtoncomp <airtonishimori@gmail.com>
2026-08-31 22:59:58 -0300

    Test strview

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <stdint.h>
 
 #include "strview.h"
-#include "strbuf.h"
 
 void test_strview()
 {
@@ -184,26 +183,9 @@ void test_strview()
     printf("--- END: TEST STRVIEW ---\n");
 }
 
-void test_strbuf()
-{
-    printf("--- BEGIN: TEST STRBUF ---\n");
-
-    strbuf_t sb1;
-    sb_init(&sb1, 100);
-    sb_print_safe_stdout(&sb1);
-
-    strbuf_t sb2;
-    sb_init_str(&sb2, "test-strbuf", strlen("test-strbuf"));
-    //sb_print_safe_stdout(&sb2);
-    sb_println_safe_stdout(&sb2);
-
-    printf("--- END: TEST STRBUF ---\n");
-}
-
 int main() 
 {
-    //test_strview();
-    test_strbuf();
+    test_strview();
 
     return 0;
 }
